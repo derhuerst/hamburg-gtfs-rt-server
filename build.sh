63b3cec6a471c9fbc3ccb187e2a02400032f08bc
Jannis R <mail@jannisr.de>
2021-01-09 15:51:06 +0100

    use HVV 2021-01-08 dataset

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -x
 
 # todo: find a URL that always points to the latest dataset
-wget -O gtfs.zip 'http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/6245d691-d86a-4059-85a0-a997e7857092/Upload__HVV_Rohdaten_GTFS_UebgFpl_20201204.zip'
+wget -O gtfs.zip 'http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/7ee59356-9bc9-4665-b9b6-01f2cd9e5353/Upload__HVV_Rohdaten_GTFS_Fpl_20210108.zip'
 unzip -o -d gtfs -j gtfs.zip
 
 env | grep '^PG'
