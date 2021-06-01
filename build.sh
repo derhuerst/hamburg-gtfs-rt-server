557fcf4f788c11f86bd73f5ed66daa2965503ea9
Jannis R <mail@jannisr.de>
2021-06-01 22:44:52 +0100

    use HVV 2021-05-12 dataset, tweak PostgreSQL import

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ set -o pipefail
 set -x
 
 # todo: find a URL that always points to the latest dataset
-wget -O gtfs.zip 'http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/612d5372-3d2e-4447-b8af-8e49c0f92f77/Upload__HVV_Rohdaten_GTFS_Fpl_20210304.zip'
+wget -O gtfs.zip 'https://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/cba27ff1-ee81-448c-a266-3de320e4c88c/Upload__HVV_Rohdaten_GTFS_Fpl_20210512.zip'
 unzip -o -d gtfs -j gtfs.zip
 
 env | grep '^PG'
 
-NODE_ENV=production node_modules/.bin/gtfs-to-sql \
+NODE_ENV=production node_modules/.bin/gtfs-to-sql -d --trips-without-shape-id --routes-without-agency-id -- \
 	gtfs/agency.txt \
 	gtfs/calendar.txt \
 	gtfs/calendar_dates.txt \
@@ -21,7 +21,7 @@ NODE_ENV=production node_modules/.bin/gtfs-to-sql \
 	gtfs/stops.txt \
 	gtfs/transfers.txt \
 	gtfs/trips.txt \
-	-d | psql -b
+	| psql -b
 
 lib="$(dirname $(realpath $0))/lib"
 NODE_ENV=production node_modules/.bin/build-gtfs-match-index \
