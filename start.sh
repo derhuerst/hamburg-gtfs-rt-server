6e791ee8e1eb8df0b155aa6b403cb37b1ec2007d
Holger Bruch <hb@mfdz.de>
2021-10-21 00:40:04 +0200

    Adds docker-compose and initial GTFS import

--- start.sh
+++ start.sh
@@ -9,6 +9,22 @@ lib="$(dirname $(realpath $0))/lib"
 # https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/2173421#2173421
 trap 'exit_code=$?; kill -- $(jobs -p); exit $exit_code' SIGINT SIGTERM EXIT
 
+# Wait until postgres is available  
+until psql -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "Postgres is up - continuing"
+
+>&2 echo "Checking if GTFS was already imported..."
+if [[ $(psql -t -c 'SELECT 1 FROM agency LIMIT 1') ]]
+then
+    >&2 echo "GTFS data already imported, skipping build.sh"
+else
+    >&2 echo "No GTFS data imported in database, running build.sh ..."
+    ./build.sh
+fi
+
 NODE_ENV=production node_modules/.bin/monitor-hafas \
 	$lib/hafas.js \
 	&
